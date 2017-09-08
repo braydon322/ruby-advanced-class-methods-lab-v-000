a8850461f74c41b670fd1d9b21c3a13556525ecf
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-08 16:55:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,8 @@ class Song
 
     @name = split_array_no_whitespace[1].join(" ")
     @artist_name = split_array_no_whitespace[0].join(" ")
-    self.new(@name)
+    save
+
   end
 
   def self.create_from_filename(file_name)
